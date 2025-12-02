b470f981f2ff28de54663ef298e853521b018c23
Viraj Mehta <viraj@tensorzero.com>
2025-12-01 21:48:51 -0500

    set up sqlx and rust

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn make_migrator() -> sqlx::migrate::Migrator {
+    sqlx::migrate!("src/postgres/migrations")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
